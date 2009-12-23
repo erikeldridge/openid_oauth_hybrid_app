21f539d012b9351e700d80093ebc2fb4141da861
erik <erikeldridge@yahoo.com>
2009-12-22 17:19:38 -0800

    removed dependencies on y! sdk

--- openid/return_to.php
+++ openid/return_to.php
@@ -3,47 +3,27 @@
 /**
  * This script exchanges oauth req token for access token and stores it,
  * and then passes the session identifier back to the init script (/index.php)
- * @copyright (c) 2009, Yahoo! Inc. All rights reserved.
- * @package http://github.com/erikeldridge/openid_oauth_hybrid_app
- * @license code licensed under the BSD License.  See /license.markdown
  */
  
-session_start();
-
 require_once '../config.inc.php';
-
-//ammend include path so we can include files consistently
-$includePath = get_include_path().PATH_SEPARATOR
-    .OPENID_INCLUDE_PATH.PATH_SEPARATOR
-    .OAUTH_INCLUDE_PATH.PATH_SEPARATOR;
-set_include_path($includePath);
-
-require_once 'Yahoo.inc';
+require('../../yosdk/OAuth.php');
 
 //safely fetch input
 $filters = array(
-    'openid.sreg.nickname' => FILTER_SANITIZE_ENCODED,
-    'openid.sreg.fullname' => FILTER_SANITIZE_ENCODED,
-    'openid.sreg.email' => FILTER_SANITIZE_ENCODED,
-    'openid.identity' => FILTER_SANITIZE_ENCODED,
-    
-    //kludge: actual param is dot-separated, but it's unreadable that way
+    'openid_sreg_nickname' => FILTER_SANITIZE_ENCODED,
+    'openid_sreg_fullname' => FILTER_SANITIZE_ENCODED,
+    'openid_sreg_email' => FILTER_SANITIZE_ENCODED,
+    'openid_identity' => FILTER_SANITIZE_ENCODED,
     'openid_oauth_request_token' => FILTER_SANITIZE_ENCODED
 );
 $input = filter_var_array($_REQUEST, $filters);
-
-//session identifier
-$sessionId = session_id();
-
-// session store interface defined in Yahoo! SDK
-$yahooSdkSessionStore = new NativeSessionStore();
 
 // exchange request token for access token
 // ref: YahooAuthorization::getAccessTokenProxy
 if(isset($input['openid_oauth_request_token'])){
 	
 	//use oauth consumer to sign request for access token
-	$consumer = new OAuthConsumer(YAHOO_OAUTH_APP_KEY, YAHOO_OAUTH_APP_SECRET);
+    $consumer = new OAuthConsumer(YAHOO_OAUTH_APP_KEY, YAHOO_OAUTH_APP_SECRET, YAHOO_OAUTH_APP_ID);
 	
 	//format request token as expected by oauth lib
 	$requestToken = new stdclass();
@@ -51,56 +31,69 @@ if(isset($input['openid_oauth_request_token'])){
 	
 	//ref: http://step2.googlecode.com/svn/spec/openid_oauth_extension/latest/openid_oauth_extension.html#AuthTokenReq
 	$requestToken->secret = '';
-
-    //client defined in Yahoo! SDK
-	$client = new OAuthClient($consumer, $requestToken, OAUTH_PARAMS_IN_POST_BODY);
 	
-	//$YahooConfig["OAUTH_HOSTNAME"] defined in Yahoo! SDK
-	$uri = sprintf("https://%s/oauth/v2/get_token", $YahooConfig["OAUTH_HOSTNAME"]);
-	
-	$response = $client->post($uri);
+	$url = 'https://api.login.yahoo.com/oauth/v2/get_token';
+    $request = OAuthRequest::from_consumer_and_token($consumer, $requestToken, 'POST', $url, array());
+    $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $requestToken);
+    $headers = array(
+    	"Accept: application/json"
+    );
 
-	parse_str($response["responseBody"], $params);
+    //make request
+    $ch = curl_init($url);
+    $options = array(
+        CURLOPT_POST=> true,
+    	CURLOPT_POSTFIELDS => $request->to_postdata(),
+		CURLOPT_HTTPHEADER => $headers,
+    	CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $options);
+    parse_str(curl_exec($ch), $response);
+    curl_close($ch);
 
 	$now = time();
-
 	$accessToken = new stdclass();
 	
-	//note: key is oauth access token.
-	//kludge: suspecting php bug - 1st array elem inaccesible by key.
-	$accessToken->key = array_shift($params);
+	//load token w/ openid params
+	$accessToken->openid = $input;
+
+    //set statndard oauth params
+	$accessToken->key = $response['oauth_token'];
+	$accessToken->secret = $response["oauth_token_secret"];
+	$accessToken->guid = $response["xoauth_yahoo_guid"];
 	
-	$accessToken->secret = $params["oauth_token_secret"];
-	$accessToken->guid = $params["xoauth_yahoo_guid"];
-	
-	//note: consumer is the app key
+	//note: consumer is the app key not consumer obj
 	$accessToken->consumer = YAHOO_OAUTH_APP_KEY;
 	
-	$accessToken->sessionHandle = $params["oauth_session_handle"];
+	$accessToken->sessionHandle = $response["oauth_session_handle"];
 
 	// Check to see if the access token ever expires.
-	if(array_key_exists("oauth_expires_in", $params)) {
-	    $accessToken->tokenExpires = $now + $params["oauth_expires_in"];
+	if(array_key_exists("oauth_expires_in", $response)) {
+	    $accessToken->tokenExpires = $now + $response["oauth_expires_in"];
 	}
 	else {
 	    $accessToken->tokenExpires = -1;
 	}
 
 	// Check to see if the access session handle ever expires.
-	if(array_key_exists("oauth_authorization_expires_in", $params)) {
+	if(array_key_exists("oauth_authorization_expires_in", $response)) {
 	    $accessToken->handleExpires = $now +
-	            $params["oauth_authorization_expires_in"];
+	            $response["oauth_authorization_expires_in"];
 	}
 	else {
 	    $accessToken->handleExpires = -1;
 	}
     
-	$yahooSdkSessionStore->storeAccessToken($accessToken);
+	file_put_contents('../token.txt', json_encode($accessToken));
 }
 
 ?>
 
+You are now logged in.  This window will close in 3 seconds.
 <script>
-var sessionId = '<?= $sessionId ?>';
+
+// communicate session state to popup monitor in index.php
+var session = 'true';
+
 window.setTimeout(window.close, 3000);
 </script>
