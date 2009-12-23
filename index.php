8e11adc3be914d190e7a196b7778e774a8a270bb
erik <erikeldridge@yahoo.com>
2009-12-22 17:18:41 -0800

    now supporting oauth and openid

--- index.php
+++ index.php
@@ -3,17 +3,23 @@
  * This is the init script.  Load this in a browser and click the log in button.   
  * After rolling through the openid flow, it'll call back to proxy.php
  * to fetch profile data, and then display profile pic and name.
- * @copyright (c) 2009, Yahoo! Inc. All rights reserved.
- * @package http://github.com/erikeldridge/openid_oauth_hybrid_app
- * @license code licensed under the BSD License.  See /license.markdown
  */ 
  -->
 
-<div style="border-bottom: 1px solid #ddd; padding: 1ex">
-    <a href="openid/?openid_identifier=yahoo.com">
-        <img src="http://l.yimg.com/a/i/reg/openid/buttons/2_new.png" style="border:none;display:none" id="loginButton"/>
-    </a>
-    <div id="profileDisplay" style="display:none">
+<div style="border-bottom: 1px solid #ddd; padding: 1ex" id="header">
+    <div style="display:none" id="auth">
+        <a href="openid/?openid_identifier=yahoo.com">
+            <img src="http://l.yimg.com/a/i/reg/openid/buttons/2_new.png" id="openid" style="border:none"/>
+        </a>
+        <p>--- or ---</p>
+        <!-- oauthmod -->
+        <a href="oauth/" id="oauth">
+            auth w/ oauth
+        </a>
+    </div>
+    <div id="profile" style="display:none">
+        <span id="name"></span> - <a href="." id="logout">logout</a><br/>
+        <img id="pic" src="http://l.yimg.com/a/i/ww/met/anim_loading_sm_082208.gif"/>
     </div>
 </div>
 
@@ -22,18 +28,24 @@
     
 YUI().use('node', 'event', 'io', 'json', 'cookie', function (Y) {
     
-    var handleIoComplete = function (id, o, args) {
+    var proxyUri = 'proxy.php',
+        handleIoComplete = function (id, o, args) {
             
             //Try/catch described in http://developer.yahoo.com/yui/3/json/#errors
             try {
                 var json = Y.JSON.parse(o.responseText);
-                //console.log(json);
-                var name = json.query.results.profile.nickname,
-                    src = json.query.results.profile.image.imageUrl;
+                Y.log(json);
                     
                 //display profile pic & nickname.
-                Y.get('#profileDisplay').append('<img src="'+src+'" height="48" width="48"/>'+name);
-                Y.get('#profileDisplay').setStyle('display', 'block');
+                Y.get('#profile').setStyle('display', 'block');
+                Y.get('#name').set('innerHTML', json.query.results.profile.nickname);
+                Y.get('#pic').set('src', json.query.results.profile.image.imageUrl);
+                
+                //show log out link
+                Y.get('#logout').on("click", function () {
+                    Y.Cookie.remove('session');
+                });
+                
             } catch (e) {
                 console.log(e);
             }
@@ -47,35 +59,36 @@ YUI().use('node', 'event', 'io', 'json', 'cookie', function (Y) {
                 popupCheckInterval = null,
     
                 //Def. fn to catch openid when auth is complete, or handle failure.
-                checkPopup = function () {
+                checkPopup = function () {console.log('foo');
                     
                     //If openid is defined, auth is complete, so ...
-            		if (popup.sessionId) {
-		    
-            			//... hide log in button, ...
-            			Y.get('#loginButton').setStyle('display', 'none');
-			            
-			            //todo: show profile loading icon while waiting
+            		if (popup.session) {
+		                
+		                //register session state
+                        Y.Cookie.set('session', 'true');
+                        
+            			//... hide openid/oauth ui, ...
+            			Y.get('#auth').setStyle('display', 'none');
 			            
             			//... fetch profile data, ...
-                        var uri = 'proxy.php?sessionId=' + popup.sessionId;
-
                         Y.on('io:complete', 
                             handleIoComplete, 
                             this                            
                         );
-                        Y.io(uri);
+                        Y.io(proxyUri);
                         
                         //... stop monitoring popup.
-            			clearInterval(popupCheckInterval);
-			
+                	    clearInterval(popupCheckInterval);
+                	    
             		//If popup handle ceases to point to anything, ...
             		} else if (Y.Lang.isUndefined(popup)) {
-            		    
-            		    //... stop monitoring.
-            		    clearInterval(popupCheckInterval);
-            		    
+            		
+            		    //... stop monitoring popup.
+                	    clearInterval(popupCheckInterval);
+                	    
             	    }
+            	    
+            	    
             	},
             	
             	//Define fn to spawn popup window for openid authentication.
@@ -98,29 +111,40 @@ YUI().use('node', 'event', 'io', 'json', 'cookie', function (Y) {
             	};
         	
         //If login button clicked ...
-        if (e.target.get('id') && ('loginButton' === e.target.get('id'))) {
+        if (e.target.get('id') && ('openid' === e.target.get('id'))) {
             
             //...cancel default action and ...
             e.halt();            
             
             //...init openid authentication in popup using href of a-tag around button.
             launchPopup(e.target.get('parentNode').get('href'));
+            
+        } else if (e.target.get('id') && ('oauth' === e.target.get('id'))) {
+
+            //...cancel default action and ...
+            e.halt();            
+
+            //...init openid authentication in popup using href of a-tag around button.
+            launchPopup(e.target.get('href'));
         }
     };
     
     //Init.
-    var sessionId = Y.Cookie.get('sessionId');
+    var session = Y.Cookie.get('session');
     
     //If cookie doesn't exist, user's not logged in and/or is not registered, so ...
-    if (!sessionId) {
-
-    	//... reveal log in link and ...
-        Y.get('#loginButton').setStyle('display', 'block');
-    }
+    if ('true' === session) {
+        Y.on('io:complete', 
+            handleIoComplete, 
+            this                            
+        );
+        Y.io(proxyUri);
+    } else {
     
-    //... let callback trigger display actions.
-    Y.on("click", handleClick);
-	
+    	//... reveal log in link and ...
+        Y.get('#auth').setStyle('display', 'block').on("click", handleClick);
+        
+    }	
 });
 
 </script>
