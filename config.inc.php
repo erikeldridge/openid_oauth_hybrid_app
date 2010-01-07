c2faa4aa1c2725a19c11108d5b9d36eb76680e00
erik <erikeldridge@yahoo.com>
2010-01-07 09:40:04 -0800

    added definition of callback

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- * This is pretty self-explanatory.  the oauth app used needs scoping for profile data.
  * @copyright (c) 2009, Yahoo! Inc. All rights reserved.
  * @package http://github.com/erikeldridge/openid_oauth_hybrid_app
  * @license code licensed under the BSD License.  See /license.markdown
@@ -10,6 +9,7 @@
 define('YAHOO_OAUTH_APP_KEY', '');
 define('YAHOO_OAUTH_APP_SECRET', '');
 define('YAHOO_OAUTH_APP_ID', '');
+define('YAHOO_OAUTH_APP_CALLBACK_URI', '');
 
 define('OPENID_REALM_URI', 'http://example.com');
 define('OPENID_RETURN_TO_URI', 'http://example.com/openid_oauth_hybrid_app/openid/return_to.php');
@@ -17,3 +17,5 @@ define('OPENID_RETURN_TO_URI', 'http://example.com/openid_oauth_hybrid_app/openi
 //include paths for libs
 define('OPENID_INCLUDE_PATH', '/path/to/openid');
 define('OAUTH_INCLUDE_PATH', '/path/to/yahoo/php/sdk');
+
+?>
